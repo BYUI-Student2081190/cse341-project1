cd7c3557c3d1a01dc0f6d3d545f0ebdd42bb8a19
byui_student1045 <sco22022@byui.edu>
2024-11-13 19:01:24 -0700

    Fixed the server.js

--- server.js
+++ server.js
@@ -9,16 +9,6 @@ const port = process.env.PORT || 3000;
 /**  Middleware  **/
 // Utilize body-parser
 app.use(bodyParser.json());
-// Allow Header Access Across Site
-// app.use((req, res, next) => {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader(
-//         'Access-Control-Allow-Headers',
-//         'Origin, X-Requested-Width, Content-Type, Accept, Z-Key'
-//     );
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-//     next();
-// });
 
 
 /**   Routes   **/
